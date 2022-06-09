d74b34302dc615074fc96114f7a1b36e91248750
samir <s-malik@live.co.uk>
2022-06-09 14:41:54 +0100

    Updated tests to have stubbed testing and extracted wordchoser logic to new class

--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -2,19 +2,34 @@ package game;
 
 import static org.junit.Assert.assertEquals;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
 
 public class GameTest {
     @Test
     public void testGetsWordToGuess() {
-        Game game = new Game("MAKERS");
-        assertEquals("M_____", game.getWordToGuess());
+        WordChoser mockedChoser = mock(WordChoser.class);
+        when(mockedChoser.getRandomWordFromDictionary()).thenReturn("DEVELOPER");
+        Game game = new Game(mockedChoser);
+        assertEquals("D________", game.getWordToGuess());
     }
 
     @Test
     public void testGetsRemainingAttempts() {
-        Game game = new Game("MAKERS"); 
+        WordChoser mockedChoser = mock(WordChoser.class);
+        when(mockedChoser.getRandomWordFromDictionary()).thenReturn("DEVELOPER");
+        Game game = new Game(mockedChoser);
         int result = Integer.valueOf(10); 
         assertEquals(result, game.getRemainingAttempts());
     }
+
+    @Test
+    public void testGetsWordToGuessWithRandomWord() {
+        WordChoser mockedChoser = mock(WordChoser.class);
+        when(mockedChoser.getRandomWordFromDictionary()).thenReturn("DEVELOPER");
+        Game game = new Game(mockedChoser);
+        assertEquals("D________", game.getWordToGuess());
+    }
 }
+
